a2f10ade3e503df5d00fc75823c63beb01e937de
Patrick Rogers <patrick@donotforget.xyz>
2019-12-08 12:44:06 -0800

    refactor(day08): use pixel type

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -1,26 +1,65 @@
-fn solve_01(input: &[u32], width: usize, height: usize) -> usize {
-  let m = input
-    .chunks_exact(width * height)
-    .min_by_key(|&c| c.iter().filter(|&&d| d == 0).count())
-    .unwrap();
+use std::fmt;
 
-  m.iter().filter(|&&d| d == 1).count() * m.iter().filter(|&&d| d == 2).count()
+#[derive(PartialEq, Clone, Copy)]
+pub enum Pixel {
+  Transparent,
+  Black,
+  White,
 }
 
-fn solve_02(input: &[u32], width: usize, height: usize) -> Vec<u32> {
+impl From<char> for Pixel {
+  fn from(c: char) -> Pixel {
+    match c {
+      '0' => Pixel::Black,
+      '1' => Pixel::White,
+      '2' => Pixel::Transparent,
+      _ => unreachable!(),
+    }
+  }
+}
+
+impl fmt::Display for Pixel {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Pixel::White => write!(f, "▒"),
+      _ => write!(f, " "),
+    }
+  }
+}
+
+impl Pixel {
+  fn paint(self, layer: Pixel) -> Pixel {
+    match self {
+      Pixel::Transparent => layer,
+      n => n,
+    }
+  }
+}
+
+fn solve_01(input: &[Pixel], width: usize, height: usize) -> usize {
+  let m = input
+    .chunks_exact(width * height)
+    .min_by_key(|c| c.iter().filter(|&&d| d == Pixel::Black).count())
+    .unwrap();
+
+  m.iter().filter(|d| **d == Pixel::White).count()
+    * m.iter().filter(|d| **d == Pixel::Black).count()
+}
+
+fn solve_02(input: &[Pixel], width: usize, height: usize) -> Vec<Pixel> {
   input
     .chunks_exact(width * height)
-    .fold(vec![2; width * height], |acc, layer| {
+    .fold(vec![Pixel::Transparent; width * height], |acc, layer| {
       acc
         .iter()
         .zip(layer)
-        .map(|(curr, layer)| if *curr == 2 { *layer } else { *curr })
+        .map(|(&curr, &layer)| curr.paint(layer))
         .collect()
     })
 }
 
-fn input_gen(input: &str) -> Vec<u32> {
-  input.chars().map(|c| c.to_digit(10).unwrap()).collect()
+fn input_gen(input: &str) -> Vec<Pixel> {
+  input.chars().map(|c| c.into()).collect()
 }
 
 pub fn solve(input: &str) {
@@ -31,8 +70,8 @@ pub fn solve(input: &str) {
   let img = solve_02(&r, 25, 6);
 
   img.chunks(25).for_each(|row| {
-    for &d in row {
-      print!("{}", if d == 0 { " " } else { "*" })
+    for &pixel in row {
+      print!("{}", pixel);
     }
     println!();
   })
