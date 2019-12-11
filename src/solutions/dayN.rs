2f49693584421bec39573013476e798092d3009b
Patrick Rogers <patrick@donotforget.xyz>
2019-12-10 18:13:19 -0800

    feat(template): add tests to template

--- src/solutions/dayN.rs
+++ src/solutions/dayN.rs
@@ -5,3 +5,18 @@ pub fn solve(input: &str) {
   dbg!(solve_01());
   dbg!(solve_02());
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn part_one() {
+    unimplemented!();
+  }
+
+  #[test]
+  fn part_two() {
+    unimplemented!();
+  }
+}
